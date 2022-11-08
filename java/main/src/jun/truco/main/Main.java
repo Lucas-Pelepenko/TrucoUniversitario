c49ed63d622b74d5b2878cff5842d3c1f24432f5
Cristina Pelepenko <pelepenko.cristina@gmail.com>
2022-11-08 19:27:54 -0300

    #Prova refatoracao1

--- java/main/src/jun/truco/main/Main.java
+++ java/main/src/jun/truco/main/Main.java
@@ -7,6 +7,8 @@ import jun.truco.model.Carta;
 import jun.truco.model.Humano;
 import jun.truco.model.Jogador;
 import jun.truco.model.Mesa;
+import jun.truco.service.Partida;
+
 import java.util.ArrayList;
 
 public class Main {
@@ -64,29 +66,14 @@ public class Main {
 			for(Jogador jogador = m.nextJogadoresVida(); jogador != null; jogador = m.nextJogadoresVida()){
 			if(m.getPartidas() == 0){
 				if(jogador instanceof Humano){
-					Boolean validador = false;
-					String resposta = "";						
-					System.out.println("Deseja ir? ('sim' ou 'nao')");
-					while(!validador){ //faz while ate que resposta informada seja uma das 2 seguintes "sim" ou "nao"
-						while(!scanner.hasNextLine()) scanner.next(); //so ira fazer as demais validacoes caso seja um string
-						resposta = scanner.nextLine();
-						if((resposta.equals("sim") || resposta.equals("nao"))){
-							validador = true;
-							jogador.setPontosPendente((resposta == "sim") ? 1 : 0); //caso faz entao pendente ira receber +1
-						}else{
-							System.out.println("Resposta invalida, Deseja ir? ('sim' ou 'nao')");
-						}
-					} 
+					Partida partida = new Partida();
+					partida.primeira_rodada(jogador);
 				}
 		    }else{
 		    	if(jogador.getNome().equals(nome)){
 		    		System.out.print("Mao: ");
 		    		for(Carta c : jogador.getMao())
 		    			System.out.print(c.toString()+", ");
-
-
-
-
 						Boolean validador = false;
 						System.out.println("\nFaz quanto?");
 						while(!validador){ //faz while ate que resposta informada seja uma das 2 seguintes "sim" ou "nao"
